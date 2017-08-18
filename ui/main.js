2ed0f7b60f3e39b0097037dd38346554b0d20f99
SadathSaad <30660807+SadathSaad@users.noreply.github.com>
2017-08-19 00:21:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
 var element = document.getElementById('rollO');
 element.innerHTML ='I am Rolling Out Here..!!';
 var imad = document.getElementsByClassName('mLg');
-var marginLt = 0;
-function movRt (){
-    marginLt = marginLt + 10;
-    imad.style.marginLeft = marginLt + 'px';
+var marginLeft = 0;
+function moveRight (){
+    marginLeft = marginLeft + 10;
+    imad.style.marginLeft = marginLeft + 'px';
 }
 imad.onclick= function (){
-    var interval = setInterval(movRt,100);
+    var interval = setInterval(moveRight,100);
 }
