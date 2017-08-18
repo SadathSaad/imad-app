001c1f27ec14cc375164b27ae372ead82e66de35
SadathSaad <30660807+SadathSaad@users.noreply.github.com>
2017-08-19 00:18:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,7 @@
 var element = document.getElementById('rollO');
 element.innerHTML ='I am Rolling Out Here..!!';
 
-var imad = document.getElementsByClassName('mLg');
-var marginLt = 0;
-function movRt (){
-    marginLt = marginLt + 10;
-    imad.style.marginLeft = marginLt + 'px';
-}
-imad.onclick= function (){
-    var interval = setInterval(movRt,100);
-}
+
 jQuery(function($) {
   
   // Function which adds the 'animated' class to any '.animatable' in view
