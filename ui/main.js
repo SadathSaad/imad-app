99b4eda53abb87ce2ae860c6ef1293df676e740d
SadathSaad <30660807+SadathSaad@users.noreply.github.com>
2017-08-18 23:54:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 var element = document.getElementById('rollO');
-alert(element.innerText);
+alert(element);
 jQuery(function($) {
   
   // Function which adds the 'animated' class to any '.animatable' in view
