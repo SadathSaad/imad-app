473a6df4dd996e58c2e061c53a3791c150bb3c50
SadathSaad <30660807+SadathSaad@users.noreply.github.com>
2017-08-19 00:20:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,34 +1,11 @@
 var element = document.getElementById('rollO');
 element.innerHTML ='I am Rolling Out Here..!!';
-
-
-jQuery(function($) {
-  
-  // Function which adds the 'animated' class to any '.animatable' in view
-  var doAnimations = function() {
-    
-    // Calc current offset and get all animatables
-    var offset = $(window).scrollTop() + $(window).height(),
-        $animatables = $('.animatable');
-    
-    // Unbind scroll handler if we have no animatables
-    if ($animatables.size() == 0) {
-      $(window).off('scroll', doAnimations);
-    }
-    
-    // Check all animatables and animate them if necessary
-		$animatables.each(function(i) {
-       var $animatable = $(this);
-			if (($animatable.offset().top + $animatable.height() - 20) < offset) {
-        $animatable.removeClass('animatable').addClass('animated');
-			}
-    });
-
-	};
-  
-  // Hook doAnimations on scroll, and trigger a scroll
-	$(window).on('scroll', doAnimations);
-  $(window).trigger('scroll');
-
-});
-
+var imad = document.getElementsByClassName('mLg');
+var marginLt = 0;
+function movRt (){
+    marginLt = marginLt + 10;
+    imad.style.marginLeft = marginLt + 'px';
+}
+imad.onclick= function (){
+    var interval = setInterval(movRt,100);
+}
