790737113686516c7a5905561d623221022ac281
Sadath Saad <30660807+SadathSaad@users.noreply.github.com>
2017-08-22 11:28:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ return htmlTemplate;
 }
 var pool = new Pool(config);
 app.get('/article/:articleName', function (req, res){
-pool.query("SELECT * FROM articles where title = '" + req.params.articleName+"'", function(err, result) {
+pool.query("SELECT * FROM articles where title = $1", req.params.articleName, function(err, result) {
 if(err){
 res.status(500).send(err.toString());
 }else{
