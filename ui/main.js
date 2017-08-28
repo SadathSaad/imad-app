d95effb9712a21efb54c783b926fe082b0e0d2d1
Sadath Saad <30660807+SadathSaad@users.noreply.github.com>
2017-08-28 10:44:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,71 @@
 
+jQuery(function($) {
+  
+  // Function which adds the 'animated' class to any '.animatable' in view
+  var doAnimations = function() {
+    
+    // Calc current offset and get all animatables
+    var offset = $(window).scrollTop() + $(window).height(),
+        $animatables = $('.animatable');
+    
+    // Unbind scroll handler if we have no animatables
+    if ($animatables.size() == 0) {
+      $(window).off('scroll', doAnimations);
+    }
+    
+    // Check all animatables and animate them if necessary
+		$animatables.each(function(i) {
+       var $animatable = $(this);
+			if (($animatable.offset().top + $animatable.height() - 20) < offset) {
+        $animatable.removeClass('animatable').addClass('animated');
+			}
+    });
+
+	};
+  
+  // Hook doAnimations on scroll, and trigger a scroll
+	$(window).on('scroll', doAnimations);
+  $(window).trigger('scroll');
+
+});
+
+var element = document.getElementById('rollO');
+var imad = document.getElementById('mLg');
+var marginLeft = 0;
+function moveRight (){
+    marginLeft = marginLeft + 10;
+    imad.style.marginLeft = marginLeft + 'px';
+    if (marginLeft == 1000){
+        marginLeft = -50;
+    }
+}
+imad.onclick= function (){
+    var interval = setInterval(moveRight,100);
+element.innerHTML ='I am Rolling Out Here..!! <i style="font-size:17px;color:red"> Click to Stop Me!!</i>';
+element.onclick=function () {
+    clearInterval(interval);
+    element.innerHTML ='Smart - stArt';
+}
+};
+var list1 = document.getElementById('l1');
+var maps = document.getElementsByClassName('map'), i;
+var list2 = document.getElementById('l2');
+var cds = document.getElementsByClassName('cd'), i;
+list1.onclick=function () {
+for (var i = 0; i < cds.length; i ++) {
+    cds[i].style.display = 'inline-block';
+}
+    for (var j = 0; j < maps.length; j ++) {
+    maps[j].style.display = 'none';}
+};
+list2.onclick=function () {
+for (var i = 0; i < cds.length; i ++) {
+    cds[i].style.display = 'none';
+}
+    for (var j = 0; j < maps.length; j ++) {
+    maps[j].style.display = 'table-cell';
+}};
+
 var clk = document.getElementById('counter');
 clk.onclick = function (){
     var request=new XMLHttpRequest();
@@ -8,25 +75,12 @@ clk.onclick = function (){
                
                 var counter = request.responseText;
                 document.getElementById('count').innerHTML = counter.toString();
+    clk.innerHTML=('Liked');
+    $(clk).attr('disabled',true);$(clk).css({'background':'black'});
             }
-        };
+        }
 };
     request.open('GET', 'http://sadathullakhaliq.imad.hasura-app.io/counter',true);
     request.send(null);
-    
-//submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
-submit.onclick=function(){
-  //make the request to the server and send the name
-  
-  //capture a list of names and render it as a list
-  var names = ['name1','name2','name3','name4'];
-  var list = '';
-  for(var i=0; i< names.length; i++){
-      list += '<li>' + names[i] + '</li>' ;
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
 };
+
