cd40801af2b1fc1c6222c4ce034a4013e393994f
Sadath Saad <30660807+SadathSaad@users.noreply.github.com>
2017-08-28 10:28:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,20 @@
-//counter code
-var button = document.getElementById('counter');
-button.oncLick = function() {
 
-//Create A Request To Counter Endpoint
-var request = new XMLHttpRequest();
-//Capture The Response And Store In HTML
-request.onreadystatechange = function() {
-if (request.readyState === XMLHttpRequest.DONE) 
-{
-//Take Some Action
-if (request.status === 200) 
- { 
-var counter= request.responseText; 
-var span = document.getElementById('count');
-span.innerHTML = counter.toString();
-   }
-  }
+var clk = document.getElementById('counter');
+clk.onclick = function (){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE) 
+            if (request.status === 200) {
+               
+                var counter = request.responseText;
+                document.getElementById('count').innerHTML = counter.toString();
+    clk.innerHTML=('Liked');
+    $(clk).attr('disabled',true);$(clk).css({'background':'black'});
+            }
+        }
 };
-//Make A Request
-request.open('GET', 'http://shubhamshekhar089.imad.hasura-app.io/counter', true);
-request.send(null);
+    request.open('GET', 'http://sadathullakhaliq.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
 //submit name
 var nameInput = document.getElementById('name');
